9f7e3c887eca2bc0794f97ab8fc8c9f885e60fc1
d0iasm <doiasami1219@gmail.com>
2018-07-22 15:18:52 +0900

    Add context property for debug

---
+++ lifegame.cpp
@@ -0,0 +1,64 @@
+#include <SDL2/SDL.h>
+#include <emscripten.h>
+#include <cstdlib>
+
+struct context {
+    SDL_Renderer *renderer;
+    int iteration;
+    int a;
+};
+
+void mainloop(void *arg) {
+    context *ctx = static_cast<context*>(arg);
+    SDL_Renderer *renderer = ctx->renderer;
+
+    // example: draw a moving rectangle
+    
+    // red background
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    
+    // moving blue rectangle
+    SDL_Rect r;
+    r.x = ctx->iteration % 255;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+    SDL_Rect r2;
+    r2.y = ctx->iteration % 255;
+    r2.x = 50;
+    r2.w = 50;
+    r2.h = 50;
+    SDL_SetRenderDrawColor(renderer, 0, 155, 50, 255);
+    SDL_RenderFillRect(renderer, &r);
+    SDL_RenderFillRect(renderer, &r2);
+
+    printf("%d \n", ctx->a);
+    ctx->a++;
+    
+    SDL_RenderPresent(renderer);
+
+    ctx->iteration++;
+}
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(255, 255, 0, &window, &renderer);
+
+    context ctx;
+    ctx.renderer = renderer;
+    ctx.iteration = 0;
+    ctx.a = 100;
+
+    const int simulate_infinite_loop = 1; // call the function repeatedly
+    const int fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
+    emscripten_set_main_loop_arg(mainloop, &ctx, fps, simulate_infinite_loop);
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
